085ab604623133931ddb9842451d3ab7857b4a25
Sttp92 <esteban.estay@ug.uchile.cl>
2024-11-01 20:48:32 -0300

    Part2 Exercises 2.11

--- part2/phonebook/phonebook/src/App.jsx
+++ part2/phonebook/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const FilterContact = ({ handleFilterChange }) => {
   return (
@@ -33,17 +34,19 @@ const Contacts = ({ contactsToShow }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '978763342', id: 1 },
-    { name: 'John Cena', number: '040-123456', id: 2 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 3 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 4 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 5 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setFilterValue] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const findBy = (value) => {
     return persons.find((person) => person.name === value)
