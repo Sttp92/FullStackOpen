f0b4ffcd91294a15a8cd92e5d61af210b5b3eb6f
Sttp92 <esteban.estay@ug.uchile.cl>
2024-11-19 20:10:21 -0300

    Change BaseUrl

--- part2/phonebook/phonebook/vite.config.js
+++ part2/phonebook/phonebook/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://fullstackopenpart3-wujz.onrender.com',
         changeOrigin: true,
       },
     }
